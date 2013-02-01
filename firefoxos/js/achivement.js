3fb50aeff93c4ebbd46b15ab6024bab3bcee9dd0
bryanyuan2 <bryanyuan2@gmail.com>
2013-02-02 01:29:25 +0800

    modify img and icon

--- firefoxos/js/achivement.js
+++ firefoxos/js/achivement.js
@@ -1,11 +1,11 @@
 $(function() {
-	var achievement=window.localStorage["achievement"];
-	var $t=0;
-	for($i=0;$i<7;$i++){
-		if(achievement[$i]=="t"){
-			console.log(achievement[$i]);
-			var $t=$i+1;
-			$("#img"+$t).attr("src","img/icon.png");
+	var achievement = window.localStorage["achievement"];
+	var t = 0;
+	for(i = 0;i < 7;i++){
+		if(achievement[i] == "t"){
+			console.log(achievement[i]);
+			var t = i + 1;
+			$("#img" + t).attr("src","img/icon.png");
 		}
 	}
 });
